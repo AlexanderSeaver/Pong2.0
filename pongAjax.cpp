3b0b028795becc3b62b8f16e0d8e4330428383be
coiregavhan <gavinhannerc@kenyon.edu>
2017-03-31 13:54:05 -0400

    wrote ajax and server to set user numbers

--- pongAjax.cpp
+++ pongAjax.cpp
@@ -1,7 +1,74 @@
+/*************************************************************/
+/* Pong2.0 CGI program                                       */
+/*************************************************************/
+
 #include <iostream>
+// Stuff for AJAX
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+
+//Stuff for pipes
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include "fifo.h"
+
 using namespace std;
+using namespace cgicc; // Needed for AJAX functions.
 
 int main()
 {
+	const GAME_STATE_PRE = "-1"; //while setting usernumbers
+	const GAME_STATE_INPLAY = "1"; //while playing the game
+	
+	//pipe information
+	string uNumPipe_ServerToAjax = "uNum_server_to_ajax"; //use this to identify different users
+	string uNumPipe_AjaxToServer = "uNum_ajax_to_server";
+	string paddlePipe_ServerToAjax = "paddle_server_to_ajax";
+	string paddlePipe_AjaxToServer = "paddle_ajax_to_server";
+		
+	string paddleRecFromServer;
+	string gameState = GAME_STATE_PRE;
+	
+	//gets userNumber and paddle position from the javascript
+	Cgicc cgiNum;    // Ajax object
+	form_iterator userNumber = cgiNum.getElement("userNumber");
+	string userno = **userNumber;	
+	if (userno != "0") 
+	{ 
+		gameState = GAME_STATE_INPLAY; 
+	}
+	Cgicc cgiPaddle;    // Ajax object	
+	form_iterator paddley = cgiPaddle.getElement("paddley");
+	string paddlePos = **paddley;
+	
+	cout << "Content-Type: text/plain\n\n";
+	
+	if(gameState == GAME_STATE_PRE) // obtain the actual user number from the server (1 or 2)
+	{
+		Fifo uNumFifo_ServerToAjax(uNumPipe_ServerToAjax);
+		Fifo uNumFifo_AjaxToServer(uNumPipe_AjaxToServer);
+		
+		uNumFifo_AjaxToServer.openwrite();
+		uNumFifo_AjaxToServer.send(userno);
+		uNumFifo_AjaxToServer.fifoclose();
+	
+		uNumFifo_ServerToAjax.openread();
+		userno = uNumFifo_ServerToAjax.recv();
+		uNumFifo_ServerToAjax.fifoclose();
+		cout << userno;	
+	}
+	else if(gameState == GAME_STATE_INPLAY)
+	{
+		
+	}
+		
+	
 	return 0;
 }
--- pongServer.cpp
+++ pongServer.cpp
@@ -1,7 +1,62 @@
+/*************************************************************/
+/* Authors: Alex Seaver and Coire Gavin-Hanner               */
+/* Date Created: 2-17-17					                 */
+/*************************************************************/
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <string>
+#include "fifo.h"
 using namespace std;
 
 int main()
 {
+	//game state information
+	const GAME_STATE_PRE = "-1"; //while setting usernumbers
+	const GAME_STATE_INPLAY = "1"; //while playing the game
+	string gameState = GAME_STATE_PRE;
+	int numberOfUsers = 0;
+	
+	//pipe information
+	string uNumPipe_ServerToAjax = "uNum_server_to_ajax"; //use this to identify different users
+	string uNumPipe_AjaxToServer = "uNum_ajax_to_server";
+	string paddlePipe_ServerToAjax = "paddle_server_to_ajax";
+	string paddlePipe_AjaxToServer = "paddle_ajax_to_server";
+	
+	// create the FIFOs for communication
+	Fifo uNumFifo_ServerToAjax(uNumPipe_ServerToAjax);
+	Fifo uNumFifo_AjaxToServer(uNumPipe_AjaxToServer);
+	Fifo paddleFifo_AjaxToServer(paddlePipe_AjaxToServer);
+	Fifo paddleFifo_ServerToAjax(paddlePipe_ServerToAjax);
+	
+	//routines
+	While (gameState == GAME_STATE_PRE)
+	{
+		uNumFifo_AjaxToServer.openread();
+		string userNo = uNumFifo_AjaxToServer.recv();
+		uNumFifo_AjaxToServer.close();
+		while (numberOfUsers < 2)
+		{
+			numberOfUsers++;
+			uNumFifo_ServerToAjax.openwrite();
+			uNumFifo_ServerToAjax.send(numberOfUsers);
+			uNumFifo_ServerToAjax.close();
+		}		
+		gameState = GAME_STATE_INPLAY;
+	}
+	
+	while (gameState == GAME_STATE_INPLAY)
+	{
+		
+	}
 	return 0;
 }
